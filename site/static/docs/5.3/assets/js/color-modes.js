72b4dd7b1d267908fcfcfddf88d15df066e7cd61
XhmikosR <xhmikosr@gmail.com>
2023-03-17 15:57:09 +0200

    Slightly improve examples PNG files
    Before: 97,4 KB (99.758 bytes)
    After:  94,2 KB (96.512 bytes)

--- site/static/docs/5.3/assets/js/color-modes.js
+++ site/static/docs/5.3/assets/js/color-modes.js
@@ -1,68 +1,63 @@
-const storageKey = 'theme-preference'
-const clipElement = document.createElement('div')
-document.addEventListener('keypress', (event) => {
-  const keyName = event.key;
-  if (keyName === 'd') {
-    onClick();
+const html = document.documentElement
+const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+const savedTheme = localStorage.getItem('theme') || systemTheme
+let animate = false
+function setTheme(theme, animate = false) {
+  if (animate) {
+    const el = document.querySelector('body')
+    const copy = el.cloneNode(true)
+    copy.classList.add('app-copy')
+    const rect = el.getBoundingClientRect()
+    copy.style.top = rect.top + 'px'
+    copy.style.left = rect.left + 'px'
+    copy.style.width = rect.width + 'px'
+    copy.style.height = rect.height + 'px'
+    const targetEl = document.activeElement
+    const targetRect = targetEl.getBoundingClientRect()
+    const left = targetRect.left + targetRect.width / 2 + window.scrollX
+    const top = targetRect.top + targetRect.height / 2 + window.scrollY
+    el.style.setProperty('--clip-pos', `${left}px ${top}px`)
+    el.style.removeProperty('--clip-size')
+    setTimeout(function () {
+      el.classList.add('app-transition')
+      requestAnimationFrame(function () {
+        requestAnimationFrame(function () {
+          el.style.setProperty('--clip-size', Math.hypot(window.innerWidth, window.innerHeight) + 'px')
+        })
+      })
+    })
+
+    document.body.append(copy)
+    const scrollElements = copy.querySelectorAll('[data-scroll-x], [data-scroll-y]');
+    scrollElements.forEach((el) => {
+      el.scrollLeft = +el.dataset.scrollX;
+      el.scrollTop = +el.dataset.scrollY;
+    });
+    const onTransitionend = function (e) {
+      if (e.target === e.currentTarget) {
+        copy.remove()
+        el.removeEventListener('transitionend', onTransitionend)
+        el.removeEventListener('transitioncancel', onTransitionend)
+        el.classList.remove('app-transition')
+        el.style.removeProperty('--clip-size')
+        el.style.removeProperty('--clip-pos')
+      }
+    }
+    el.addEventListener('transitionend', onTransitionend)
+    el.addEventListener('transitioncancel', onTransitionend)
   }
-});
-const onClick = () => {
-  theme.value = theme.value === 'light'
-    ? 'dark'
-    : 'light'
-  setPreference()
+  document.documentElement.setAttribute('data-bs-theme', theme)
+  localStorage.setItem('theme', theme)
 }
 
-const storedTheme = localStorage.getItem('theme')
-const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+setTheme(savedTheme)
+window.addEventListener('DOMContentLoaded', () => {
+  const themeToggle = document.querySelector('#themeToggle')
 
-const getColorPreference = () => {
-  if (storedTheme) {
-    return storedTheme
-  }
-
-  return prefersDarkMode ? 'dark' : 'light'
-}
-
-// const getColorPreference = () => {
-//   if (localStorage.getItem(storageKey))
-//     return localStorage.getItem(storageKey)
-//   else
-//     return localStorage.setItem('theme-preference', 'light')
-//   return window.matchMedia('(prefers-color-scheme: dark)').matches
-//     ? 'dark'
-//     : 'light'
-// }
-const setPreference = () => {
-  localStorage.setItem(storageKey, theme.value)
-  reflectPreference()
-  addTransition()
-}
-
-const reflectPreference = () => {
-  document.firstElementChild.setAttribute('data-bs-theme', theme.value)
-  document.querySelector('#theme-toggler')?.setAttribute('aria-label', theme.value)
-}
-const theme = {
-  value: getColorPreference(),
-}
-reflectPreference()
-window.onload = () => {
-  reflectPreference()
-  if (document.querySelector('#theme-toggler') !== null) {
-    document.querySelector('#theme-toggler').addEventListener('click', onClick)
-    document.body.classList.add('app-transition');
-  }
-}
-const addTransition = () => {
-  clipElement.classList.add('clip-transition')
-  if(clipElement)
-    document.body.prepend(clipElement);
-  setTimeout(function () {
-    document.body.classList.remove('app-transition')
-  }, 1000);
-}
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', ({matches: isDark}) => {
-  theme.value = isDark ? 'dark' : 'light'
-  setPreference()
+  themeToggle.addEventListener('click', () => {
+    const currentTheme = localStorage.getItem('theme')
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark'
+    localStorage.setItem('theme', newTheme)
+    setTheme(newTheme, true) // передаємо параметр animate як true
+  })
 })
